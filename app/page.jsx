6daada1a8d7577eb7bc1d322fdc007de76bde42e
Võ Quang Chiến <87252943+2giosangmitom@users.noreply.github.com>
2024-01-28 16:46:08 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 	return (
 		<>
 			<header className="sm:mt-5 mt-4">
-				<h1 className="xl:text-2xl xl:font-bold sm:text-lg">
+				<h1 className="xl:text-2xl xl:font-bold text-lg font-semibold">
 					Year progress - {month} / {year}
 				</h1>
 			</header>
