940921bd60bf880e0dd0bdc886a9189e2b26a887
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-28 13:48:59 +0700

    feat: update year progress

--- components/YearProgress.jsx
+++ components/YearProgress.jsx
@@ -1,17 +1,28 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getYearProgress } from "@/utils/year";
 
 export default function YearProgress() {
 	const yearProgressRef = useRef();
+	const requestRef = useRef();
+	const [progress, setProgress] = useState(0);
+
+	const animate = () => {
+		setProgress(getYearProgress());
+		requestRef.current = requestAnimationFrame(animate);
+		yearProgressRef.current.style.width = `${progress}%`;
+	};
 
 	useEffect(() => {
-		yearProgressRef.current.style.width = getYearProgress();
-	}, []);
+		requestRef.current = requestAnimationFrame(animate);
+		return () => {
+			cancelAnimationFrame(animate);
+		};
+	}, [animate]);
 
 	return (
 		<div>
-			<p>Year progress: {getYearProgress()}</p>
+			<p>Year progress: {progress.toFixed(5)}%</p>
 			<div className="bg-black w-full h-2">
 				<div
 					className="bg-yellow-700 h-full w-0 duration-1000"
