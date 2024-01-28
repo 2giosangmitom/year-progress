cfd7968160b82537a45585d105a27fdfcefe3c91
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-01-28 14:08:03 +0700

    fix: correct algorithms

--- components/DayProgress.jsx
+++ components/DayProgress.jsx
@@ -1,17 +1,28 @@
 "use client";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { getDayProgress } from "@/utils/day";
 
 export default function DayProgress() {
 	const dayProgressRef = useRef();
+	const [progress, setProgress] = useState(0);
+	const requestRef = useRef();
+
+	const animate = () => {
+		setProgress(getDayProgress());
+		requestRef.current = requestAnimationFrame(animate);
+		dayProgressRef.current.style.width = `${progress}%`;
+	};
 
 	useEffect(() => {
-		dayProgressRef.current.style.width = getDayProgress();
-	}, []);
+		requestRef.current = requestAnimationFrame(animate);
+		return () => {
+			cancelAnimationFrame(animate);
+		};
+	}, [animate]);
 
 	return (
 		<div>
-			<p>Day progress: {getDayProgress()}</p>
+			<p>Day progress: {progress.toFixed(5)}%</p>
 			<div className="bg-black w-full h-2">
 				<div
 					className="bg-yellow-700 h-full w-0 duration-1000"
