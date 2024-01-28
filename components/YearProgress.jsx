d36144937a6d9bbc1eeb468de129bb6ef2c9864d
Vo Quang Chien <voquangchien.dev@proton.me>
2024-01-28 11:36:21 +0700

    feat: year progress

--- components/YearProgress.jsx
+++ components/YearProgress.jsx
@@ -1,45 +1,6 @@
 "use client";
 import { useEffect, useRef } from "react";
-
-/**
- * Check leap year
- *
- * @param {number} year
- */
-function isLeapYear(year) {
-	return (year % 100 === 0 && year % 400 === 0) || year % 4 === 0
-		? true
-		: false;
-}
-
-/**
- * Check number of days of a year
- *
- * @param {number} year
- */
-function numOfDays(year) {
-	return isLeapYear(year) ? 366 : 365;
-}
-
-/**
- * Get current day of year
- */
-function dayOfYear() {
-	const now = new Date();
-	const start = new Date(now.getFullYear(), 0, 0);
-	const diff =
-		now -
-		start +
-		(start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000;
-	const oneDay = 1000 * 60 * 60 * 24;
-	const dayOfYear = Math.floor(diff / oneDay);
-	return dayOfYear;
-}
-
-/** Get year progress */
-function getYearProgress(day, numOfDays) {
-	return `${((day / numOfDays) * 100).toFixed(2)}%`;
-}
+import { dayOfYear, daysInYear, getYearProgress } from "@/utils/year";
 
 export default function YearProgress() {
 	const date = new Date();
@@ -48,13 +9,13 @@ export default function YearProgress() {
 	useEffect(() => {
 		yearProgressRef.current.style.width = getYearProgress(
 			dayOfYear(),
-			numOfDays(date.getUTCFullYear()),
+			daysInYear(date.getUTCFullYear()),
 		);
 	}, [date]);
 
 	return (
 		<div>
-			<p>Year progress: {getYearProgress(dayOfYear(), numOfDays())}</p>
+			<p>Year progress: {getYearProgress(dayOfYear(), daysInYear())}</p>
 			<div className="bg-black w-full h-2">
 				<div
 					className="bg-yellow-700 h-full w-0 duration-1000"
