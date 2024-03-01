5dd100cfb84c9c67e0f0351e75bfc6fcddcf12da
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-03-01 22:38:42 +0700

    refactor: improve code readability

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -1,63 +1,72 @@
 "use client";
 import React, { useEffect, useState, createContext, useContext } from "react";
 
-type Theme = "light" | "dark";
+export type Theme = "light" | "dark";
 
-type ThemeContextType = {
-	theme: Theme;
-	toggleTheme: () => void;
+export type ThemeContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
 };
 
 const ThemeContext = createContext<ThemeContextType | null>(null);
 
-export default function ThemeContextProvider({ children }: { children: React.ReactNode }) {
-	const [theme, setTheme] = useState<Theme>("light");
+export default function ThemeContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [theme, setTheme] = useState<Theme>("light");
 
-	const toggleTheme = () => {
-		if (theme === "light") {
-			setTheme("dark");
-			window.localStorage.setItem("theme", "dark");
-			document.documentElement.classList.add("dark");
-		} else {
-			setTheme("light");
-			window.localStorage.setItem("theme", "light");
-			document.documentElement.classList.remove("dark");
-		}
-	};
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
 
-	useEffect(() => {
-		const localTheme = window.localStorage.getItem("theme") as Theme | null;
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as Theme | null;
 
-		if (localTheme) {
-			setTheme(localTheme);
+    if (localTheme) {
+      setTheme(localTheme);
 
-			if (localTheme === "dark") {
-				document.documentElement.classList.add("dark");
-			}
-		} else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-			setTheme("dark");
-			document.documentElement.classList.add("dark");
-		}
-	}, []);
+      if (localTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      }
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
 
-	return (
-		<ThemeContext.Provider
-			value={{
-				theme,
-				toggleTheme,
-			}}
-		>
-			{children}
-		</ThemeContext.Provider>
-	);
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
-export function useTheme() {
-	const context = useContext(ThemeContext);
+/**
+ * A custom hook to access the theme context.
+ * @returns {ThemeContextType} The current theme context.
+ * @throws Will throw an error if used outside of a ThemeContextProvider.
+ */
+export function useTheme(): ThemeContextType {
+  const context = useContext(ThemeContext);
 
-	if (context === null) {
-		throw new Error("useTheme must be used within a ThemeContextProvider");
-	}
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeContextProvider");
+  }
 
-	return context;
+  return context;
 }
--- lib/utils/month.ts
+++ lib/utils/month.ts
@@ -1,10 +1,13 @@
 import { getDayProgress } from "./day";
 
-/** Get month progress */
-export function getMonthProgress() {
-	const now = new Date();
-	const totalDaysOfMonth = new Date(now.getFullYear(), now.getMonth(), 0).getDate();
-	const progressPercentage = ((now.getDate() - 1 + getDayProgress() / 100) / totalDaysOfMonth) * 100;
+/**
+ * Get the progress of the current month in percentage.
+ * @returns The progress of the current month in percentage.
+ */
+export function getMonthProgress(): number {
+  const now: Date = new Date();
+  const totalDaysOfMonth: number = new Date(now.getFullYear(), now.getMonth(), 0).getDate();
+  const progressPercentage: number = ((now.getDate() - 1 + getDayProgress() / 100) / totalDaysOfMonth) * 100;
 
-	return progressPercentage;
+  return progressPercentage;
 }
