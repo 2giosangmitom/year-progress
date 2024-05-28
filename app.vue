61e74467796234fab12a390cec601709496e74bb
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-05-28 20:59:14 +0700

    chore: update

--- app.vue
+++ app.vue
@@ -8,6 +8,9 @@ useHead({
   bodyAttrs: {
     class: "xl:px-20 lg:px-16 md:px-10 sm:px-5 px-4 bg-zinc-300 dark:bg-stone-950 dark:text-slate-50 transition",
   },
+  htmlAttrs: {
+    lang: "en",
+  },
 });
 
 const progress = useProgressStore();
--- tests/daysInYear.test.ts
+++ tests/daysInYear.test.ts
@@ -6,7 +6,7 @@ describe("daysInYear function", () => {
     { year: 2000, expected: 366, description: "the year 2000 (leap year divisible by 400)" },
     { year: 1900, expected: 365, description: "the year 1900 (non-leap year divisible by 100 but not by 400)" },
     { year: 2100, expected: 365, description: "the year 2100 (non-leap year not divisible by 400)" },
-    { year: 2001, expected: 365, description: "the year 2001 (non-leap year)" }
+    { year: 2001, expected: 365, description: "the year 2001 (non-leap year)" },
   ];
 
   testCases.forEach(({ year, expected, description }) => {
@@ -17,7 +17,7 @@ describe("daysInYear function", () => {
 
   const errorTestCases = [
     { year: -2022, errorMessage: "Year cannot be negative", description: "negative year values" },
-    { year: 2022.5, errorMessage: "Year must be an integer", description: "non-integer year values" }
+    { year: 2022.5, errorMessage: "Year must be an integer", description: "non-integer year values" },
   ];
 
   errorTestCases.forEach(({ year, errorMessage, description }) => {
@@ -26,4 +26,3 @@ describe("daysInYear function", () => {
     });
   });
 });
-
