f89b2f7d538395b140a4e7c90cfa26d27aa43438
Vo Quang Chien <yeuxacucodon.dev@proton.me>
2024-05-28 20:10:26 +0700

    feat: toggle dark/light theme

--- components/Progress.vue
+++ components/Progress.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
 type Title = "Year" | "Month" | "Day" | "Hour";
 
-defineProps<{
+const props = defineProps<{
   title: Title;
-  progress: number; update: void;
+  progress: number;
 }>();
 </script>
 
-<template></template>
+<template>
+  <div class="xl:my-0 my-5">
+    <p class="mb-2">{{ props.title }}: {{ props.progress.toFixed(5) }}%</p>
+    <div
+      class="border border-black shadow-lg w-full h-5 dark:border-slate-50"
+      role="progressbar"
+      :aria-valuenow="props.progress"
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <div class="bg-slate-900 h-full w-0 duration-1000 dark:bg-stone-300" :style="{ width: `${props.progress}%` }" />
+    </div>
+  </div>
+</template>
