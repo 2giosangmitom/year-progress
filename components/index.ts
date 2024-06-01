d4cc703307595985e5e15b6e952d280a02f0924d
Vo Quang Chien <voquangchien.dev@proton.me>
2024-06-01 12:57:34 +0700

    refactor: reorganise structure

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export { default as TheFooter } from "./TheFooter.vue";
+export { default as TheHeader } from "./TheHeader.vue";
+export { default as TheProgress } from "./TheProgress.vue";
+export { default as ToggleTheme } from "./ToggleTheme.vue";
